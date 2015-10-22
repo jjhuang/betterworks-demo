5e22f15d1c20edccbfcbd20383f04d8e11ae538b
Justin Huang <jhuang@bitglass.com>
2015-10-22 09:27:34 -0700

    Tweaks

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,13 +9,13 @@ ctrl.controller('ChartCtrl', function($scope) {
         {'expected': 0.1, 'actual': 1},
         {'expected': 2, 'actual': -3},
     ];
-    $scope.expected = 0.5;
-    $scope.actual = 0.9;
+    $scope.expected = Math.round(Math.random() * 100) / 100;
+    $scope.actual = Math.round(Math.random() * 100) / 100;
 });
 
 ctrl.directive("progressChart", function() {
     return {
-        restrict: "EA",
+        restrict: "E",
         link: function(scope, elem, attrs) {
             var radius = 45;
             var pi = Math.PI;
@@ -26,20 +26,20 @@ ctrl.directive("progressChart", function() {
 
             // Render the outer arc
             var actualArc = d3.svg.arc().innerRadius(radius + 17).outerRadius(radius + 11).cornerRadius(5).startAngle(0);
-            var actualPath = group.append('path').datum({endAngle: 0.001}).attr('fill', "#77C000").attr('d', actualArc);
+            var actualPath = group.append('path').datum({endAngle: 1e-6}).attr('fill', "#77C000").attr('d', actualArc);
 
             // Render the inner arc
             var expectArc = d3.svg.arc().innerRadius(radius + 9).outerRadius(radius + 6).cornerRadius(5).startAngle(0);
-            var expectPath = group.append('path').datum({endAngle: 0.001}).attr('fill', "#c4e493").attr('d', expectArc);
+            var expectPath = group.append('path').datum({endAngle: 1e-6}).attr('fill', "#c4e493").attr('d', expectArc);
 
             // Render the XX% Progress text
             var text = group.append("text").attr('class', 'actualText')
                                            .attr('dx', '-0.3em')
                                            .text(0)
                                            .transition()
-                                           .duration(750)
+                                           .duration(1000)
                                            .tween("text", function(d) {
-                                                var i = d3.interpolate(this.textContent, 100 * scope.actual);
+                                                var i = d3.interpolate(this.textContent, 100 * attrs.actual);
                                                 return function(t) {
                                                     this.textContent = Math.round(i(t));
                                                 };
@@ -47,12 +47,12 @@ ctrl.directive("progressChart", function() {
             group.append("text").text("%").attr('class', 'percentText').attr('dx', '1.5em');
             group.append("text").text("Progress").attr('class', 'progressText').attr('dy', '1.1em');
 
-            expectPath.transition().duration(750).call(arcTween, expectArc, scope.expected);
-            actualPath.transition().duration(750).call(arcTween, actualArc, scope.actual);
+            expectPath.transition().call(arcTween, expectArc, attrs.expected);
+            actualPath.transition().call(arcTween, actualArc, attrs.actual);
 
             function arcTween(transition, arc, newVal) {
-                transition.attrTween("d", function(d) {
-                    var interpolate = d3.interpolate(d.endAngle, pi * 2 * newVal);
+                transition.duration(1000).attrTween("d", function(d) {
+                    var interpolate = d3.interpolate(d.endAngle, 1e-6 + pi * 2 * newVal);
                     return function(t) {
                         d.endAngle = interpolate(t);
                         return arc(d);
