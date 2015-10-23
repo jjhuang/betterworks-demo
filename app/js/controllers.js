4b54d7897210dbba78e2e2a338b77216d9f3c037
Justin Huang <jhuang@bitglass.com>
2015-10-23 09:13:30 -0700

    Some of Di's suggestions. (#1, #2, #3, #4, #5)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,14 +6,14 @@ var ctrl = angular.module('betterworksControllers', []);
 ctrl.controller('ChartCtrl', function($scope) {
     // Test data
     $scope.chartData = [
-        {'id': 1, 'expected': 0.5, 'actual': 0.25},
-        {'id': 2, 'expected': 0.1, 'actual': 1},
-        {'id': 3, 'expected': 2, 'actual': -3},
-        {'id': 4, 'expected': 0.5, 'actual': NaN},
-        {'id': 5, 'expected': NaN, 'actual': 0.35},
-        {'id': 6, 'expected': 'expected', 'actual': 'actual'},
-        {'id': 7, 'expected': '0.75', 'actual': '0.55'},
-        {'id': 8, 'expected': Number.NEGATIVE_INFINITY, 'actual': Number.POSITIVE_INFINITY}
+        {id: 1, expected: 0.5, actual: 0.25},
+        {id: 2, expected: 0.1, actual: 1},
+        {id: 3, expected: 2, actual: -3},
+        {id: 4, expected: 0.5, actual: NaN},
+        {id: 5, expected: NaN, actual: 0.35},
+        {id: 6, expected: 'expected', actual: 'actual'},
+        {id: 7, expected: 0.75, actual: 0.55},
+        {id: 8, expected: Number.NEGATIVE_INFINITY, 'actual': Number.POSITIVE_INFINITY}
     ];
 
     // Controlled by input fields
@@ -21,43 +21,45 @@ ctrl.controller('ChartCtrl', function($scope) {
     $scope.actual = Math.round(Math.random() * 100) / 100;
 });
 
-ctrl.directive("progressChart", function() {
+ctrl.directive('progressChart', function() {
     return {
-        restrict: "E",
-        require: "?ngModel",
+        restrict: 'E',
+        require: '?ngModel',
         scope: {expectVal:'=', actualVal:'='},
         link: function(scope, elem, attrs) {
+            // Constants
             var radius = 45;
+            var actualBaseArcInner = 17;
+            var actualBaseArcOuter = 11;
+            var expectBaseArcInner = 9;
+            var expectBaseArcOuter = 6;
             var pi = Math.PI;
+            var endAngle = 1e-6;  // Eliminate D3 arc path JS error from 0 to 0
+
             var chart = d3.select(elem[0]);
-            var svg = chart.append("svg").attr("width", "150").attr("height", "150");
-            var group = svg.append("g").attr('transform', 'translate(75,75)');
-            var circle = group.append("circle").attr("class", "progressCircle").attr("r", radius);
+            var svg = chart.append('svg').attr('class', 'svgContainer');
+            var group = svg.append('g').attr('transform', 'translate(75,75)');
+            var circle = group.append('circle').attr('class', 'progressCircle').attr('r', radius);
 
             // Render the outer arc
-            var actualArc = d3.svg.arc().innerRadius(radius + 17).outerRadius(radius + 11).cornerRadius(5).startAngle(0);
-            var actualPath = group.append('path').datum({endAngle: 1e-6}).attr('fill', "#77C000").attr('d', actualArc);
+            var actualArc = d3.svg.arc().innerRadius(radius + actualBaseArcInner).outerRadius(radius + actualBaseArcOuter).cornerRadius(3).startAngle(0);
+            var actualPath = group.append('path').datum({endAngle: endAngle}).attr('d', actualArc);
 
             // Render the inner arc
-            var expectArc = d3.svg.arc().innerRadius(radius + 9).outerRadius(radius + 6).cornerRadius(5).startAngle(0);
-            var expectPath = group.append('path').datum({endAngle: 1e-6}).attr('fill', "#c4e493").attr('d', expectArc);
+            var expectArc = d3.svg.arc().innerRadius(radius + expectBaseArcInner).outerRadius(radius + expectBaseArcOuter).cornerRadius(3).startAngle(0);
+            var expectPath = group.append('path').datum({endAngle: endAngle}).attr('d', expectArc);
 
             // Render the XX% Progress text
             var text = group.append("text").attr('class', 'actualText')
                                            .attr('dx', '0.45em')
                                            .attr('dy', '5px')
                                            .text(0)
-            group.append("text").text("%").attr('class', 'percentText').attr('dx', '1.5em').attr('dy', '5px');
-            group.append("text").text("Progress").attr('class', 'progressText').attr('dy', '20px');
+            group.append('text').text('%').attr('class', 'percentText').attr('dx', '1.5em').attr('dy', '5px');
+            group.append('text').text('Progress').attr('class', 'progressText').attr('dy', '20px');
 
             // Update values
             update(scope.expectVal, scope.actualVal);
-            scope.$watch('expectVal', function(newValue, oldValue) {
-                if (newValue != oldValue) {
-                    update(scope.expectVal, scope.actualVal);
-                }
-            });
-            scope.$watch('actualVal', function(newValue, oldValue) {
+            scope.$watchGroup(['expectVal', 'actualVal'], function(newValue, oldValue) {
                 if (newValue != oldValue) {
                     update(scope.expectVal, scope.actualVal);
                 }
@@ -72,7 +74,7 @@ ctrl.directive("progressChart", function() {
             }
 
             function textTween(transition, newVal) {
-                transition.duration(1000).tween("text", function(d) {
+                transition.duration(1000).tween('text', function(d) {
                     var i = d3.interpolate(this.textContent, 100 * newVal);
                     return function(t) {
                         this.textContent = Math.round(i(t));
@@ -81,14 +83,17 @@ ctrl.directive("progressChart", function() {
             }
 
             function arcTween(transition, arc, newVal) {
-                transition.duration(1000).attrTween("d", function(d) {
+                transition.duration(1000).attrTween('d', function(d) {
                     var interpolate = d3.interpolate(d.endAngle, 1e-6 + pi * 2 * newVal);
                     return function(t) {
                         d.endAngle = interpolate(t);
                         return arc(d);
                     };
-                }).attrTween("fill", function(d) {
-                    var interpolate = d3.interpolateHsl('#B00', '#6C0');
+                }).attrTween('fill', function(d) {
+                    var start = document.querySelector('.colorStart');
+                    var end = document.querySelector('.colorEnd');
+                    var interpolate = d3.interpolateHsl(getComputedStyle(start).getPropertyValue('fill'),
+                                                        getComputedStyle(end).getPropertyValue('fill'));
                     return function(t) {
                         return interpolate(newVal);
                     };
